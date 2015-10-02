e0de79405110a61b4103e4cf08d3d4ddada01acc
Peter Tripp <petertripp@gmail.com>
2015-10-02 02:23:01 -0700

    Quiet output from malformed dates, switch ignore_columns to set, remove failed attempt to catch a spurious UserWarning.

--- crunchbase-csv.py
+++ crunchbase-csv.py
@@ -6,21 +6,15 @@ def crunchbase_csv_export(filename):
     """Convert crunchbase_export.xlsx to individual CSVs"""
 
     print "Reading from Excel Workbook '%s' (please wait...)" % filename
-    try:
-        workbook = openpyxl.load_workbook(filename=filename)
-    # Quiets the (likely) harmless warning openpyxl emits:
-    # UserWarning: Discarded range with reserved name
-    except UserWarning:
-        pass
-
+    workbook = openpyxl.load_workbook(filename=filename)
     for sheet_name in ['Companies', 'Rounds', 'Investments', 'Acquisitions', 'Additions']:
         sheet = workbook[sheet_name]
         header = [k.value for k in sheet.rows[0]]
         # skip empty and reduced precision date columns
-        ignore_columns = [None, 'quarter_str', 'year_str,'
+        ignore_columns = {None, 'quarter_str', 'year_str,'
             'acquired_month', 'acquired_quarter', 'acquired_year',
             'founded_month', 'founded_quarter', 'founded_year',
-            'funded_month', 'funded_quarter', 'funded_year']
+            'funded_month', 'funded_quarter', 'funded_year'}
         with open('%s.csv' % sheet_name, 'wb') as f:
             csv_out = unicodecsv.writer(f)
             for row in sheet.rows:
@@ -29,7 +23,7 @@ def crunchbase_csv_export(filename):
                     # There must be a simpler way to get the column header for a cell
                     if header[ord(cell.column)-65] not in ignore_columns:
                         if isinstance(cell.value, basestring) and cell.value.startswith('1000-'):
-                            print "%s: Likely invalid date: %s" % (cell.coordinate, cell.value)
+                            #print "%s: Likely invalid date: %s" % (cell.coordinate, cell.value)
                             val = None
                         elif hasattr(cell.value, 'date'):
                             val = cell.value.date() # converts datetime to date
